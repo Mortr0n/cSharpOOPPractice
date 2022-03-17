b8befbdd6301b4753b03f923d8de4d637a285b78
Chris G. Morton <chris.g.morton@elmutility.com>
2022-03-17 10:25:53 -0600

    initial commit, Human mostly done

---
+++ Human/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Human
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Human chris = new Human("Chris");
+            Human jiraiya = new Human("Jiraiya", 2, 5, 4, 110);
+            Human.Attack(chris, jiraiya);
+        }
+    }
+
+    class Human
+    {
+        public string Name;
+        public int Strength;
+        public int Intelligence;
+        public int Dexterity;
+        private int health;
+
+        public int Health
+        {
+            get { return Health; }
+        }
+
+        public Human(string name)
+        {
+            {
+                Name = name;
+                Strength = 3;
+                Intelligence = 3;
+                Dexterity = 3;
+                health = 100;
+            }
+        }
+
+        public Human(string name, int str, int intel, int dex, int hlth)
+        {
+            Name = name;
+            Strength = str;
+            Intelligence = intel;
+            Dexterity = dex;
+            health = hlth;
+        }
+
+        public static void Attack( Human humanToAttack)
+        {
+            int damage = Strenth * 5;
+            humanToAttack.health -= damage;
+            Console.WriteLine($"{Name} attacked {humanToAttack.Name} and did {damage} damage.  {humanToAttack.Name} is now at {humanToAttack.health} health! ");
+        }
+
+    }
+}
---
+++ initialOop/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace initialOop
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Vehicle myVehicle = new Vehicle();
+        }
+
+        public class Vehicle
+        {
+            int MaxNumPassengers;
+            string Color;
+            double MaxSpeed;
+
+            void MakeNoise(string noise)
+            {
+                Console.WriteLine(noise);
+            }
+
+            void MakeNoise()
+            {
+                Console.WriteLine("Vroom!");
+            }
+
+            // Somevehicle.ColorProp => Color
+            // SomeVehicle.ColorProp = "Some Color"
+            string ColorProp
+            {
+                get
+                {
+                    return Color;
+                }
+                set
+                {
+                    Color = value;
+                }
+            }
+        }
+    }
+}
